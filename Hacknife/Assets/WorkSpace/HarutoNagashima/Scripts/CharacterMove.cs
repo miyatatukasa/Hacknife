f16d14a49c32f0eba2ae9bc90d78ac8c4adafe46
HarutoNagashima <19d3170013nh@edu.tech.ac.jp>
2021-06-26 16:14:35 +0900

    不要ファイル・スクリプト削除

--- Hacknife/Assets/WorkSpace/HarutoNagashima/Scripts/CharacterMove.cs
+++ Hacknife/Assets/WorkSpace/HarutoNagashima/Scripts/CharacterMove.cs
@@ -1,33 +1,34 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CapsuleCollider))]
-[RequireComponent(typeof(Rigidbody))]
 public class CharacterMove : MonoBehaviour
 {
     // 移動速度
     [SerializeField]
     float MoveSpeed;
-    // カプセルコライダの参照
-    private CapsuleCollider col;
-    // リジッドボディの参照
-    private Rigidbody rb;
-    // カプセルコライダの移動量
+//     // カプセルコライダの参照
+//     private CapsuleCollider col;
+//     // リジッドボディの参照
+//     private Rigidbody rb;
+     // カプセルコライダの移動量
     private Vector3 velocity;
-
+    // スクリプトの参照
+    [SerializeField]
+    private PlayerManagement playerManagement;
+    // 現在操作しているキャラクター
+    private GameObject nowPlayCharacter;
 
     void Start()
     {
-        // カプセルコライダ―コンポーネントを取得する
-        col = GetComponent<CapsuleCollider>();
-        // リジッドボディコンポーネントを取得する
-        rb = GetComponent<Rigidbody>();
-    }
+        playerManagement = GetComponent<PlayerManagement>();
 
+        nowPlayCharacter = playerManagement.NOW_PLAYCHARACTER;
+    }
     void FixedUpdate()
     {
+        nowPlayCharacter = playerManagement.NOW_PLAYCHARACTER;
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
 
         // キャラクター移動処理
         velocity = new Vector3(h, 0, v);    // 上下のキー入力からZ軸の移動量を取得
@@ -41,9 +42,7 @@ public class CharacterMove : MonoBehaviour
         // モデルのローカル空間での方向に変換
         velocity = transform.TransformDirection(velocity);
 
-        //上下のキー入力でキャラクターを移動させる
-        transform.localPosition += velocity * Time.fixedDeltaTime;
-        // 左右のキー入力でキャラクタをY軸で旋回させる
-        //transform.Rotate(0, h * rotateSpeed, 0);
+        // キー入力でキャラクターを移動させる
+        nowPlayCharacter.transform.localPosition += velocity * Time.fixedDeltaTime; // ここでnowPlayCharacterがnullになる キャラクター増やして試す
     }
 }

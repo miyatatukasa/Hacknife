4593b432226925ec2771dfceaf299f31fa5e6b12
HarutoNagashima <19d3170013nh@edu.tech.ac.jp>
2021-08-20 12:17:02 +0900

    コードパターンに則した書式に修正/不要スクリプト削除

--- Hacknife/Assets/WorkSpace/HarutoNagashima/Scripts/CameraController.cs
+++ Hacknife/Assets/WorkSpace/HarutoNagashima/Scripts/CameraController.cs
@@ -4,23 +4,22 @@ public class CameraController : MonoBehaviour
 {
     // カメラの変数
     [SerializeField]
-    GameObject mainCamera;
+   private GameObject mainCamera;
     // スクリプトの参照
     [SerializeField]
-    PlayerInfo playerInfo;
+   private PlayerInfo playerInfo;
     // 構え時のレティクル
     [SerializeField]
-    GameObject reticle;
+   private GameObject reticle;
     // 追従対象のオブジェクト
-    GameObject targetObj;
+   private GameObject targetObj;
     // 構え時のカメラ位置
-    GameObject ADSPos;
+   private GameObject adsPos;
 
     /// <summary>
     /// カメラの位置と追従対象の初期化
     /// </summary>
-    public void InitCameraPos()
-    {
+    public void InitCameraPos(){
         // 通常位置オブジェクト参照
         targetObj = playerInfo.PlayerObj.transform.Find("CamPos").gameObject;
         // 鎌叡智オブジェクト参照
@@ -29,27 +28,22 @@ public class CameraController : MonoBehaviour
         mainCamera.transform.position = targetObj.transform.position;
     }
 
-    private void ADSControl()
-    {
-        mainCamera.transform.position = ADSPos.transform.position;
+    private void ADSControl(){
+        mainCamera.transform.position = adsPos.transform.position;
         reticle.SetActive(true);
     }
 
-    void Start()
-    {
+    void Start(){
         InitCameraPos();
     }
 
 
-    void FixedUpdate()
-    {
+    void FixedUpdate(){
         mainCamera.transform.position += targetObj.transform.position - mainCamera.transform.position;
-        if (Input.GetAxis("ADS") >= 0.1)
-        {
+        if (Input.GetAxis("ADS") >= 0.1){
             ADSControl();
         }
-        else
-        {
+        else{
             reticle.SetActive(false);
         }
 
--- Hacknife/Assets/WorkSpace/Niitsuma/Scripts/Players/PlayerControl.cs
+++ Hacknife/Assets/WorkSpace/Niitsuma/Scripts/Players/PlayerControl.cs
@@ -16,8 +16,7 @@ public class PlayerControl : MonoBehaviour
     private Vector3 velocity;
     Rigidbody rigidbody;
 
-    void Awake()
-    {
+    void Awake(){
         _info = PlayerInfo.Instance;
         // 最初のプレイヤー
         _info.PlayerObj = _playObj;
@@ -25,8 +24,7 @@ public class PlayerControl : MonoBehaviour
         animator = _playObj.GetComponent<Animator>();
     }
 
-    void MovePlayer(GameObject player)
-    {
+    void MovePlayer(GameObject player){
         /*長島追加*/
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
@@ -41,37 +39,8 @@ public class PlayerControl : MonoBehaviour
         {
            player.transform.rotation = Quaternion.LookRotation(moveForward);
         }
-
-        /* // キャラクター移動処理
-         velocity = new Vector3(h, 0, v);    // 上下のキー入力からZ軸の移動量を取得
-
-         if (v > 0.1 || v < 0.1)
-         {
-             velocity *= MoveSpeed;       // 移動速度を掛ける
-         }
-         if (velocity.magnitude > 0.1)
-         {
-             // アニメーターに速度を渡す
-             animator.SetFloat("Speed", velocity.magnitude);
-             // 入力がある方向にキャラクターを移動させる
-             player.transform.localPosition += velocity;
-         }
-         else
-         {
-             animator.SetFloat("Speed", 0f);
-         }
-
-         // キャラクター回転処理
-         Vector3 diff = player.transform.position - playerPos;
-         if (diff.magnitude > 0.01f)
-         {
-             player.transform.rotation = Quaternion.LookRotation(diff);
-         }*/
     }
-    void FixedUpdate()
-    {
-        /*playerPos = _playObj.transform.position;
-        Debug.Log(playerPos);*/
+    void FixedUpdate(){
         MovePlayer(PlayerInfo.Instance.PlayerObj);
     }
 }
